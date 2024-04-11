ee31044998eab9014fba2d23c74794dd68729374
justic <1131918725@qq.com>
2024-04-11 15:57:29 +0800

    feat：manage init

---
+++ manage/src/components/Aside.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref, reactive } from "vue";
+</script>
+
+<template>
+  <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
+    <el-menu-item index="2">
+      <el-icon><icon-menu /></el-icon>
+      <span>Navigator Two</span>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<style lang="scss" scoped>
+.el-menu-vertical-demo {
+  height: 100%;
+}
+</style>
---
+++ manage/src/components/Header.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { ref, reactive } from "vue";
+</script>
+
+<template>
+  <div class="header">header</div>
+</template>
+
+<style lang="scss" scoped></style>
